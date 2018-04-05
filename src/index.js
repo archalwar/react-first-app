4f8fe7199891ad8ee99a22753d367c8c4f0a2675
archalwar <amarnath.archalwar@gmail.com>
2018-04-05 07:30:26 +0530

    'Components'

--- src/index.js
+++ src/index.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+/*First component created*/
 
-setInterval(function(){
+function Cartoon(props){
+	return <h1>Hello, {props.name} age {props.age}</h1>
+
+}
+
+
+
+//component using class
+/*
+class Cartoon extends React.Component{
+	render (){
+		return <h1>Hello, {this.props.name}</h1>
+	}
+
+}
+
+*/
+
+function Show(){
+	return <div>
+		<Cartoon name='Goutham' age= '25'/>
+		<Cartoon name='Samyuktha' age = '2'/>
+		</div>
+}
+
+
 
 	ReactDOM.render(
 
-	<h1>Local Time Now:- { new Date().toLocaleTimeString()} </h1>,
+	<Show />,
 	document.getElementById('root')
 
 	)
 
-}
 
-, 1000)
